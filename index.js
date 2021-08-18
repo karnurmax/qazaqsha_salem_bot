4f3feac15ca415bfd0710a76ba2dc41aadb0403d
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2021-08-18 12:01:21 +0600

    created question generator function and wrote chess rating algorithm

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ app.post(`/bot${TOKEN}`, (req, res) => {
   res.sendStatus(200);
 });
 
+function generateQuestions() {
+  //будущие вопросы будут генерироваться в зависимости от ответов на первые (сложность)
+  for(let i = 0; i< 10 ; i++) {
+    bot.sendMessage(msg.chat.id,`Question #${i}`)
+  }
+}
+
 bot.on(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, "Hello! What Would You Like To Do ^_^", {
@@ -66,6 +73,72 @@ bot.on(/\/start/, (msg) => {
     }),
   });
 });
+
+bot.on("Start Test", (msg) => {
+  //   нужно создать функцию которая сгенерирует 10 вопросов в зависимости от уровня пользователя
+  generateQuestions()
+  bot.sendMessage(msg.chat.id, `First Question! What is HTML?`, {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "Hyper Type Markup Linguistics",
+            callback_data: "HTML1",
+          },
+        ],
+        [
+          {
+            text: "Hyper Text Markup Language",
+            callback_data: "HTML2",
+          },
+        ],
+        [
+          {
+            text: "Hydro Type Marketing Language",
+            callback_data: "HTML3",
+          },
+        ],
+        [
+          {
+            text: "Hyper Type Mars Language",
+            callback_data: "HTML4",
+          },
+        ],
+      ],
+    },
+  });
+  bot.on("callback_query", (callbackQuery) => {
+    const msg = callbackQuery.message;
+    if (msg == "HTML2") {
+      bot
+        .answerCallbackQuery(callbackQuery.id) //бот обязан ответить answerCallbackQuery чтобы кнопки пропали с сообщения
+        .then(() => bot.sendMessage(msg.chat.id, "YOU ARE RIGHT!"));
+    } else {
+      bot
+        .answerCallbackQuery(callbackQuery.id)
+        .then(() => bot.sendMessage(msg.chat.id, "you are wrong!"));
+    }
+  });
+});
+
+bot.on("My Account", (msg) => {
+  //generate unique ID
+  //Show user's balance
+  bot.sendMessage(msg.chat.id, `=============\nAccound ID: 20319\nBalance: 0 KZT`)
+});
+
+bot.on("Help", (msg) => {
+  bot.sendMessage(
+    msg.chat.id,
+    "There's no help available currenty, PM to @cheenv"
+  );
+});
+
+bot.on("About", (msg) => {
+  bot.sendMessage(msg.chat.id, "With love from KZ");
+});
+
+bot.on("Donate", (msg) => {});
 
 // Start Express Server
 app.listen(port, () => {
