f7ee64b55bc09fd4f0f1f04bcef951ba618bb022
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2021-08-18 11:17:58 +0600

    created welcome page

--- index.js
+++ index.js
@@ -4,17 +4,19 @@
  */
 /* eslint-disable no-console */
 
-const TOKEN = process.env.TELEGRAM_TOKEN || '1748785167:AAEfjVDBAQXYBGGlMw1B_rEzuGv6FnH0Bf0';
-const url = 'https://qazaqsha-salem-bot.herokuapp.com';
-const port = process.env.PORT||5000;
+const TOKEN =
+  process.env.TELEGRAM_TOKEN ||
+  "1748785167:AAEfjVDBAQXYBGGlMw1B_rEzuGv6FnH0Bf0";
+const url = "https://qazaqsha-salem-bot.herokuapp.com";
+const port = process.env.PORT || 5000;
 
-const TelegramBot = require('node-telegram-bot-api');
-const express = require('express');
+const TelegramBot = require("node-telegram-bot-api");
+const express = require("express");
 
 // No need to pass any parameters as we will handle the updates with Express
 const bot = new TelegramBot(TOKEN);
 
-// This informs the Telegram servers of the new webhook.
+// This informs the Telegram servers of the new webhook. Вроде лучше устанавливать вручную через Postman
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
 const app = express();
@@ -28,13 +30,44 @@ app.post(`/bot${TOKEN}`, (req, res) => {
   res.sendStatus(200);
 });
 
+bot.on(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Hello! What Would You Like To Do ^_^", {
+    reply_markup: JSON.stringify({
+      keyboard: [
+        [
+          {
+            text: "Start Test",
+          },
+        ],
+        [
+          {
+            text: "My Account",
+          },
+        ],
+        [
+          {
+            text: "Help",
+          },
+        ],
+        [
+          {
+            text: "About",
+          },
+        ],
+        [
+          {
+            text: "Donate",
+          },
+        ],
+      ],
+      resize_keyboard: true,
+      one_time_keyboard: true, //кнопка пропадает после нажатия
+    }),
+  });
+});
+
 // Start Express Server
 app.listen(port, () => {
   console.log(`Express server is listening on ${port}`);
-});
-
-// Just to ping!
-bot.on('message', msg => {
-  console.log(msg);
-  bot.sendMessage(msg.chat.id, `received msg: ${JSON.stringify(msg, null, '\t')}`);
 });
