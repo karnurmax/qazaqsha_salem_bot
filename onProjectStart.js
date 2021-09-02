a3348dabc3865c3ea8c66cbcdc3c76b6b7b84b5f
karnurmax <karnurmax@mail.ru>
2021-09-02 21:38:05 +0600

    run test queries to mongoDb

--- onProjectStart.js
+++ onProjectStart.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const NewChatMember = require('./db/models/NewChatMember')
 async function connect() {
   await mongoose.connect(process.env.MONGODB_URL);
 }
@@ -7,6 +7,15 @@ async function connect() {
 async function run() {
   try {
     await connect()
+    const newChatMember = new NewChatMember({userId:'testUserId', questionText: 'what is your name ?', expiredAt: new Date()});
+    newChatMember.save(function (err) {
+      if (err){
+        console.log('error on creating test rows:',err)
+      }else{
+        console.log('successful craeted new test rows')
+      }
+      
+    })
     console.log('mongoDb connected')
   }
   catch (err) {
